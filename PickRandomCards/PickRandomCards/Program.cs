9ba7097b0e37fd2f497908fca1b1debfd135467c
Samuel Santos Barreto Pereira <samucateclas@gmail.com>
2021-03-03 23:15:27 -0300

    Progam Finished

--- PickRandomCards/PickRandomCards/Program.cs
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -10,7 +10,21 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            Console.ReadLine();
         }
     }
 }
